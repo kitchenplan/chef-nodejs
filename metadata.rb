9a1b6279f02d67283655b12d29d9bbdb08341151
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-02-08 10:42:00 +0100

    Only support osx

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,6 @@ description      "Installs nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-%w{ ubuntu debian mac_os_x }.each do |os|
-  supports os
-end
+supports mac_os_x
 
-depends "apt"
 depends "homebrew"
